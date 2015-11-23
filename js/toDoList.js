b20a6abebc6d92e69a6ba06ce66251b63de977f7
ceckenrode <chriseckenrode@me.com>
2015-11-23 18:35:40 -0500

    commiting changes

--- js/toDoList.js
+++ js/toDoList.js
@@ -22,13 +22,12 @@ $(document).ready(function(){
     if (checkDuplicates === false) {
     var newTd = $("<td>").append(inputValue);
     var newTr  = $("<tr>").append(newTd);
-    var btnStr = "<td><button class='btn btn-primary complete'><span class='glyphicon glyphicon-ok'></span></button>";
-    btnStr += "<button class='btn btn-danger remove'><span class='glyphicon glyphicon-remove'></span></button></td>";
+    var btnStr = "<td><button class='btn btn-danger remove'><span class='glyphicon glyphicon-remove'></span></button>";
+    btnStr += "<button class='btn btn-primary complete'><span class='glyphicon glyphicon-ok'></span></button></td>";
     newTr.append(btnStr);
     $("#outputTable").prepend(newTr);
     $("#toBeAdded").val('').focus();
   }
-    //var listLength = $("#outputTable tr").length;
     
 
   });
